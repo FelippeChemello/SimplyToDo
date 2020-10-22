d6c7837dea7db649245c770f5df333eb81095045
Felippe <felippe.chemello@kinghost.com.br>
2020-10-22 18:12:55 -0300

    Testing deploy on vercel

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.NODE_ENV == 'development' ? 'http://localhost:3333' : 'https://apis.codestack.me',
+    baseURL: process.env.NODE_ENV == 'development' ? 'http://localhost:3333' : 'https://apis.codestack.me'
 })
 
 export default api
